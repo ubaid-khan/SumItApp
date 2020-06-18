1adf894f0f4ba9385faac0d7e7a0898a91c9345e
ubaid-khan <itsubaidkhan8@gmail.com>
2020-06-18 11:26:09 +0530

    Functions added: 1-Incorrect answer will result in negative score. 2-setClickable() added on buttons 3-startTimer() functionality added.

--- app/src/main/java/com/example/sumitup/MainActivity.java
+++ app/src/main/java/com/example/sumitup/MainActivity.java
@@ -1,38 +1,42 @@
 package com.example.sumitup;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 import java.util.ArrayList;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button startButton,button0,button1,button2,button3,playAgainButton;
-    TextView exprTextView,resultTextView,scoreTextView,timerTextView;
+    Button startButton, button0, button1, button2, button3, playAgainButton;
+    TextView exprTextView, resultTextView, scoreTextView, timerTextView;
+    int locationOfCorrectAnswer, score = 0, noOfQuestions = 0;
+    ConstraintLayout mainConstraintLayout;
     ArrayList<Integer> answersArrayList = new ArrayList<Integer>();
-    int locationOfCorrectAnswer,score = 0 ,noOfQuestions = 0;
 
-    public void generateQuestion(){
+    public void generateQuestion() {
         Random rand = new Random();
         int a = rand.nextInt(21);
         int b = rand.nextInt(21);
         int incorrectAnswer;
+
         exprTextView.setText(a + " + " + b);
-        locationOfCorrectAnswer  = rand.nextInt(4); //0 1 2 3
+        locationOfCorrectAnswer = rand.nextInt(4); //0 1 2 3
         answersArrayList.clear();       //to clear previous contents to answer arraylist
-        for(int i = 0; i < 4; i++)              //adding correct answer
+        for (int i = 0; i < 4; i++)              //adding correct answer
         {
-            if(i == locationOfCorrectAnswer) {
+            if (i == locationOfCorrectAnswer) {
                 answersArrayList.add(a + b);
-            }
-            else{
+            } else {
                 incorrectAnswer = rand.nextInt(21);
-                while(incorrectAnswer == a + b) {
+                while (incorrectAnswer == a + b) {
                     incorrectAnswer = rand.nextInt(41);
                 }
                 answersArrayList.add(incorrectAnswer);
@@ -42,6 +46,11 @@ public class MainActivity extends AppCompatActivity {
         button1.setText(Integer.toString(answersArrayList.get(1)));
         button2.setText(Integer.toString(answersArrayList.get(2)));
         button3.setText(Integer.toString(answersArrayList.get(3)));
+
+        button0.setClickable(true);
+        button1.setClickable(true);
+        button2.setClickable(true);
+        button3.setClickable(true);
     }
     public void chooseAnswer(View view){
         if (view.getTag().toString().equals(Integer.toString(locationOfCorrectAnswer))) {
@@ -51,13 +60,14 @@ public class MainActivity extends AppCompatActivity {
             generateQuestion();
         }
         else{
+            score--;
             resultTextView.setText("Incorrect");
             resultTextView.setVisibility(View.VISIBLE);
             generateQuestion();
         }
         noOfQuestions++;
         scoreTextView.setText(Integer.toString(score) + "/" + Integer.toString(noOfQuestions));
-        generateQuestion();
+        generateQuestion(); //generate a new question
         }
 
         public void playAgain(View view){
@@ -71,23 +81,36 @@ public class MainActivity extends AppCompatActivity {
             startTimer();
         }
         public void startTimer(){
-            new CountDownTimer(30000,1000)  //30 seconds timer
+            new CountDownTimer(30200, 1000) //30 seconds timer
             {
                 @Override
                 public void onTick(long millisUntilFinished) {
-                    timerTextView.setText(String.valueOf(millisUntilFinished/1000 + " s"));
+                    timerTextView.setText(String.valueOf(millisUntilFinished / 1000 + " s"));
                 }
+
                 @Override
                 public void onFinish() {
                     timerTextView.setText("0 s");
                     resultTextView.setText("Your score : " + Integer.toString(score) + " / " + Integer.toString(noOfQuestions));
+                    setButtons(false);
                     playAgainButton.setVisibility(View.VISIBLE);
                 } //countdown for 30sec and tick at every 1s
             }.start();
         }
-    public void start(View view){
+
+    public void start(View view) {
+        mainConstraintLayout.setVisibility(ConstraintLayout.VISIBLE);
         startButton.setVisibility(View.INVISIBLE);
     }
+
+    public void setButtons(boolean enabled) {
+        button0.setClickable(enabled);
+        button1.setClickable(enabled);
+        button2.setClickable(enabled);
+        button3.setClickable(enabled);
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -98,14 +121,17 @@ public class MainActivity extends AppCompatActivity {
         resultTextView = findViewById(R.id.resultTextView);
         scoreTextView = findViewById(R.id.scoreTextView);
         timerTextView = findViewById(R.id.timerTextVieiw);
+        mainConstraintLayout = findViewById(R.id.mainConstraintLayout);
 
         button0 = findViewById(R.id.button0);
         button1 = findViewById(R.id.button1);
         button2 = findViewById(R.id.button2);
         button3 = findViewById(R.id.button3);
         playAgainButton = findViewById(R.id.playAgainbutton);
+
         generateQuestion();
         startTimer();
         playAgain(findViewById(R.id.playAgainbutton));
+
     }
 }
